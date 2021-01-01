88e87a27c34ade4b37f86059b0780899bb345bdf
Luis Gabriel Gomez <luis.gg1990@gmail.com>
2021-01-01 20:42:12 -0300

    Updates. Add egardienne's theme

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 cp ./templates/html/ZeroFiveEight_luisgg.html ~/.local/share/zim/templates/html/ZeroFiveEight_luisgg.html
+cp ./templates/html/SimpleWeb_luisgg.html ~/.local/share/zim/templates/html/SimpleWeb_luisgg.html
+cp ./templates/html/SimpleWeb_luisgg/style.css ~/.local/share/zim/templates/html/SimpleWeb_luisgg/style.css
